7d783d2f635e7efb88c016fe6fed1fd671262a4d
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-08 20:44:18 +0300

    add views, migration

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,110 @@
 from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from airport.models import (
+    Airport,
+    Airplane,
+    AirplaneType,
+    Crew,
+    Country,
+    Flight,
+    Order,
+    Route,
+    Ticket,
+)
+from airport.permissions import IsAdminOrIfAuthenticatedReadOnly
+
+from airport.serializers import (
+    CountrySerializer,
+    AirportSerializer,
+    RouteDetailSerializer,
+    RouteSerializer,
+    AirplaneSerializer,
+    AirplaneTypeSerializer,
+    CrewSerializer,
+    FlightDetailSerializer,
+    FlightSerializer,
+    OrderDetailSerializer,
+    OrderSerializer,
+    RouteListSerializer,
+    AirplaneListRetrieveSerializer,
+    FlightListSerializer,
+)
+
+
+class CountryViewSet(ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class AirportViewSet(ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class RouteViewSet(ModelViewSet):
+    queryset = Route.objects.all()
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return RouteDetailSerializer
+        elif self.action == "list":
+            return RouteListSerializer
+        return RouteSerializer
+
+
+class AirplaneTypeViewSet(ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+    serializer_class = AirplaneTypeSerializer
+
+
+class AirplaneViewSet(ModelViewSet):
+    queryset = Airplane.objects.all()
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return AirplaneListRetrieveSerializer
+        return AirplaneSerializer
+
+
+class CrewViewSet(ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class FlightViewSet(ModelViewSet):
+    queryset = Flight.objects.all()
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return FlightDetailSerializer
+        elif self.action == "list":
+            return FlightListSerializer
+        return FlightSerializer
+
+
+class OrderPagination(PageNumberPagination):
+    page_size = 10
+    max_page_size = 100
+
+
+class OrderViewSet(ModelViewSet):
+    queryset = Order.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return OrderDetailSerializer
+        return OrderSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
